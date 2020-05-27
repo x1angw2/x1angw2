2af5789b1892349a232307f613d102179e32398c
x1angw2 <958810150@qq.com>
2020-05-27 13:48:30 +0800

    3

--- Python3/dev/turtle/5_star.py
+++ Python3/dev/turtle/5_star.py
@@ -1,33 +1,11 @@
 import turtle
-turtle.setup(800,400,300,300)
-turtle.pensize(5)
-turtle.pencolor("red")
+turtle.setup(1000,500,50,50)
+turtle.pensize(6)
+turtle.pencolor("gold")
 turtle.pendown()
-turtle.left(72)
-turtle.fd(160)
-turtle.left(-144)
-for i in range(10):
-	turtle.fd(160)
-
+for i in range(25):
+	turtle.left(72)
+	turtle.fd(80)
 	turtle.left(-144)
-	turtle.fd(160)
-
-	turtle.left(-144)
-	turtle.fd(160)
-
-	turtle.left(-144)
-	turtle.fd(160)
-
-
-	turtle.left(-144)
-
-
-
-
-
-
-
-
-
-
+	turtle.fd(80)
 turtle.done()
