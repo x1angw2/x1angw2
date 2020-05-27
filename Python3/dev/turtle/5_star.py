f0bfcc9c86e8d57a13caff340377480798c01836
x1angw2 <958810150@qq.com>
2020-05-27 10:54:25 +0800

    2020-05-27

---
+++ Python3/dev/turtle/5_star.py
@@ -0,0 +1,33 @@
+import turtle
+turtle.setup(800,400,300,300)
+turtle.pensize(5)
+turtle.pencolor("red")
+turtle.pendown()
+turtle.left(72)
+turtle.fd(160)
+turtle.left(-144)
+for i in range(10):
+	turtle.fd(160)
+
+	turtle.left(-144)
+	turtle.fd(160)
+
+	turtle.left(-144)
+	turtle.fd(160)
+
+	turtle.left(-144)
+	turtle.fd(160)
+
+
+	turtle.left(-144)
+
+
+
+
+
+
+
+
+
+
+turtle.done()
