420bbf09503a66eeb4b61e3b5c3cd3513d277ded
x1angw2 <958810150@qq.com>
2020-05-28 10:31:13 +0800

    3

--- Python3/dev/TextProBarV1.py
+++ Python3/dev/TextProBarV1.py
@@ -5,6 +5,6 @@ for i in range(scale+1):
 	a = '*' * i
 	b = '.' * (scale - i)
 	c = (i/scale)*100
-	print("{:^3.0f}%[{}->{}]".format(c,a,b))
-	time.sleep(0.1)
-print("结束")
+	print("\r{:^3.0f}%[{}->{}]".format(c,a,b),end="")
+	time.sleep(1.0)
+print("\r         结束         ")
